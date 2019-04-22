36e7ad300f3b003f927600b92bc09aa7dc23a4f4
buffalo <1076829179@qq.com>
2019-04-22 11:04:13 +0800

    second commit

--- insertionSort.go
+++ insertionSort.go
@@ -2,23 +2,23 @@ package main
 
 import (
    "fmt"
+   "time"
 )
 
 func insertionSort(s []int) {
+   t := time.Now()
    var a = make([]int, len(s))
    copy(a, s)
    for i := 1; i < len(a); i++ {
       m := a[i]
       for j := i; j > 0; j-- {
-         if a[j-1] > m {
-            a[j] = a[j-1]
-         }
-         if a[j-1] <= m {
-            a[j] = m
-            break
+         if a[j-1] > a[i] && (j-2 < 0 || a[j-2] <= a[i]) { 
+            copy(a[j:i+1], a[j-1:i])
+            a[j-1] = m
          }
       }
    }
    fmt.Print("\n", "Figures insertionSorted:")
    printSlice(a)
+   fmt.Print("\n",time.Since(t))
 }
