4cb05ec7c603875186fd836572899aff7d85bab1
buffalo <1076829179@qq.com>
2019-04-23 14:23:39 +0800

    fourth commit

--- ShellSort.go
+++ ShellSort.go
@@ -1,22 +1,22 @@
 package main
 
-
+import (
+	"fmt"
+	"time"
+)
 
 func ShellSort(s []int) {
+	t := time.Now()
 	var a []int = make([]int, len(s))
 	copy(a, s)
 	for k := len(a) / 2; k >= 1; k /= 2 {
 		for i := 0; i < len(a); i++ {
-			m := a[i]
-			for j := i; j > 0; j -= k {
-				if (j-k > 0 && a[j-k] <= m) && m < a[j] {
-					for l := i; l > j; l -= k {
-						a[l] = a[l-k]
-					}
-					a[j] = m
-				}
+			for j := i - k; j >= 0 && a[j+k] < a[j]; j -= k {
+				a[j], a[j+k] = a[j+k], a[j]
 			}
 		}
 	}
+	fmt.Print("\n","Figures ShellSorted:")
 	printSlice(a)
+	fmt.Print("\n",time.Since(t))
 }
